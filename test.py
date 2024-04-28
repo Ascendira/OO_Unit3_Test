ee801833a124a5b9fd3382c069dc4bda15bf9790
Ascendira <112840576+Ascendira@users.noreply.github.com>
2024-04-28 10:59:44 +0800

    设定参数生成更精确的数据

--- test.py
+++ test.py
@@ -1,33 +1,45 @@
 import subprocess
 
-max_test_num = 1
+max_test_num = 100
 
 data_generator_path = 'check\dataGenerator.jar'
 check_jar_path = 'check\oo_homework9_check.jar'
 stdin_path = 'check\stdin.txt'
 check_output_path = 'check\stdout.txt'
 set_parameter_path = 'check\set_parameter.txt'
-test_jar_path = 'oohomework_2024_22371027_hw_9.jar'
+test_jar_path = [
+    'D:\\Files\\Code\\IDEA\\second year down\\OO\\Unit_3\\homework9_mutual_test\\天权星\\out\\artifacts\\_jar\\天权星.jar',
+    'D:\\Files\\Code\\IDEA\\second year down\\OO\\Unit_3\\homework9_mutual_test\\天枢星\\out\\artifacts\\_jar\\天枢星.jar',
+    'D:\\Files\\Code\\IDEA\\second year down\\OO\\Unit_3\\homework9_mutual_test\\天玑星\\out\\artifacts\\_jar\\天玑星.jar',
+    'D:\\Files\\Code\\IDEA\\second year down\\OO\\Unit_3\\homework9_mutual_test\\天璇星\\out\\artifacts\\_jar\\天璇星.jar',
+    'D:\\Files\\Code\\IDEA\\second year down\\OO\\Unit_3\\homework9_mutual_test\\开阳星\\out\\artifacts\\_jar\\开阳星.jar',
+    'D:\\Files\\Code\\IDEA\\second year down\\OO\\Unit_3\\homework9_mutual_test\\摇光星\\out\\artifacts\\_jar\\摇光星.jar',
+    'D:\\Files\\Code\\IDEA\\second year down\\OO\\Unit_3\\homework9_mutual_test\\玉衡星\\out\\artifacts\\_jar\\玉衡星.jar']
 test_output_path = 'stdout.txt'
 
 # 参数设置
 # 总指令数
 commandsNum = 3000
-# 范围 [1,10]
+# 范围 [0,10]
+# 生成ln指令的可能性
+lnPro = 10
+# ln指令生成的人数
+lnNum = 150
+# 范围 [0,10]
 # 生成ln指令时，无效value(值为0)可能性
 valueUnEffec = 5
-# 范围 [5,15]
+# 范围 [0,10]
 # 生成除了复合指令的指令时，正确指令（不报异常）可能性
 currectCommand = 10
 # 参数反应对应指令的占比，如下参数的作用会相互影响
 # ap指令举例：
 # 实际ap指令的占比 apPro / 如下参数和
 # = (7 / ( 7 + 7 + 7 + 3 + 3 + 1 + 1))
-apPro = 7
-arPro = 7
-mrPro = 7
-qvPro = 3
-qciPro = 3
+apPro = 1
+arPro = 1
+mrPro = 1
+qvPro = 1
+qciPro = 1
 qbsPro = 1
 qtsPro = 1
 
@@ -43,6 +55,8 @@ def set_parameters():
         fl.write(str(qciPro) + '\n')
         fl.write(str(qbsPro) + '\n')
         fl.write(str(qtsPro) + '\n')
+        fl.write(str(lnPro) + '\n')
+        fl.write(str(lnNum) + '\n')
 
 def compare_files(file1, file2):
     lineNum = 1
@@ -73,7 +87,7 @@ def main():
     set_parameters()
     while (cur_test_num < max_test_num):
         cur_test_num += 1
-        print("test num: ", cur_test_num)
+        print("cur_test_num num: ", cur_test_num)
         with open(set_parameter_path, 'r') as input_file, open(stdin_path, 'w') as output_file:
             data_generator_process = subprocess.Popen(['java', '-jar', data_generator_path], stdin=input_file, stdout=output_file,
                                                       stderr=subprocess.STDOUT)
@@ -82,15 +96,17 @@ def main():
             check_process = subprocess.Popen(['java', '-jar', check_jar_path], stdin=input_file, stdout=output_file,
                                              stderr=subprocess.STDOUT)
             check_process.wait()
+        for i in range(7):
+            with open(stdin_path, 'r') as input_file, open(test_output_path, 'w') as output_file:
+                test_process = subprocess.Popen(['java', '-jar', test_jar_path[i]], stdin=input_file, stdout=output_file,
+                                                stderr=subprocess.STDOUT)
+                test_process.wait()
+            print("test name: ", i + 1)
+            if compare_files(check_output_path, test_output_path):
+                print("\033[32;47mAccepted!\033[0m")
+            else:
+                print("\033[31;43mWrong Answer!\033[0m")
+                break
 
-        with open(stdin_path, 'r') as input_file, open(test_output_path, 'w') as output_file:
-            test_process = subprocess.Popen(['java', '-jar', test_jar_path], stdin=input_file, stdout=output_file,
-                                            stderr=subprocess.STDOUT)
-            test_process.wait()
-        if compare_files(check_output_path, test_output_path):
-            print("\033[32;47mAccepted!\033[0m")
-        else:
-            print("\033[31;43mWrong Answer!\033[0m")
-            break
 
 main()
